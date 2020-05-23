d9d1476572bada16179154de979bf5b4642c323c
Ivan Jerzabek <jerzabek.ivan@gmail.com>
2020-05-23 19:20:52 +0200

    popravio GH pages

---
+++ build/precache-manifest.61f7c749f645b676e3f8840eed6afc36.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b95bb3852afcdf3a090fe70052fb5555",
+    "url": "/nsous-karantena/index.html"
+  },
+  {
+    "revision": "f0224e556354cf679343",
+    "url": "/nsous-karantena/static/css/main.4be4479a.chunk.css"
+  },
+  {
+    "revision": "022323c111eae81cfe98",
+    "url": "/nsous-karantena/static/js/2.53bd4d33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nsous-karantena/static/js/2.53bd4d33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0224e556354cf679343",
+    "url": "/nsous-karantena/static/js/main.a0c3971b.chunk.js"
+  },
+  {
+    "revision": "b2d33c5bccf46fc6f509",
+    "url": "/nsous-karantena/static/js/runtime-main.ca576e04.js"
+  },
+  {
+    "revision": "687d33c96591a441fb00ac8f5ca7c950",
+    "url": "/nsous-karantena/static/media/dora.687d33c9.jpg"
+  },
+  {
+    "revision": "6d0cc882ad7607b257d64aa5dfc8f5f8",
+    "url": "/nsous-karantena/static/media/ezekijel.6d0cc882.jpg"
+  },
+  {
+    "revision": "24a816ffeb52954ddb84fcd7c81ba27c",
+    "url": "/nsous-karantena/static/media/file.24a816ff.jpg"
+  },
+  {
+    "revision": "f8221f9af2987c402c4e62cfa75f59fc",
+    "url": "/nsous-karantena/static/media/ivona.f8221f9a.jpg"
+  },
+  {
+    "revision": "5add7c71713a6e0d51bc449711a86030",
+    "url": "/nsous-karantena/static/media/jarza.5add7c71.jpg"
+  },
+  {
+    "revision": "d47a7ee0d7663791b6063ddc93943d69",
+    "url": "/nsous-karantena/static/media/lorena.d47a7ee0.jpg"
+  },
+  {
+    "revision": "f6cef6edfe12f6b74106827dfa3a2a67",
+    "url": "/nsous-karantena/static/media/lovro.f6cef6ed.jpg"
+  },
+  {
+    "revision": "8ac111c8a51968e125fe58f167977143",
+    "url": "/nsous-karantena/static/media/morana.8ac111c8.jpg"
+  },
+  {
+    "revision": "3553a22586339a1ce9e9a03c95c6402c",
+    "url": "/nsous-karantena/static/media/petra.3553a225.jpg"
+  },
+  {
+    "revision": "265f2847308b0c1555112c256784a3f7",
+    "url": "/nsous-karantena/static/media/valentino.265f2847.jpg"
+  }
+]);
