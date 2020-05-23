b834c6598038cab4ebef26767a1431c7d6151e55
Ivan Jerzabek <jerzabek.ivan@gmail.com>
2020-05-23 19:41:56 +0200

    Popravio footer spacing

--- src/Footer.js
+++ src/Footer.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className='px-5 d-flex justify-content-around align-items-center footer'>
+    <div className='px-5 d-flex flex-lg-row flex-column justify-content-around align-items-center footer'>
       <p>2020 © Ivan Jeržek</p>
-      <a className='bijelo' href='https://jarza.media/' rel="noopener noreferrer" target='_blank'>jarza.media</a>
-      <p>Shoutout NSOUS</p>
+      <a className='my-5 bijelo' href='https://jarza.media/' rel="noopener noreferrer" target='_blank'>jarza.media</a>
+      <p className=''>Shoutout NSOUS</p>
     </div>
   );
 };
