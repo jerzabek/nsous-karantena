7660e5af8d0249fbec8b1a16ea8462799c96b480
Ivan Jerzabek <jerzabek.ivan@gmail.com>
2020-05-23 19:12:05 +0200

    Dodao build

---
+++ precache-manifest.0dacd3107e1527ab33631330bbc2c6a6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4596fdb25c0980565856287371533d55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34746b3dd4f35a24d880",
+    "url": "/static/css/main.4be4479a.chunk.css"
+  },
+  {
+    "revision": "022323c111eae81cfe98",
+    "url": "/static/js/2.53bd4d33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.53bd4d33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34746b3dd4f35a24d880",
+    "url": "/static/js/main.e3cc2030.chunk.js"
+  },
+  {
+    "revision": "fa80c0b819accd8e0146",
+    "url": "/static/js/runtime-main.168906f8.js"
+  },
+  {
+    "revision": "687d33c96591a441fb00ac8f5ca7c950",
+    "url": "/static/media/dora.687d33c9.jpg"
+  },
+  {
+    "revision": "6d0cc882ad7607b257d64aa5dfc8f5f8",
+    "url": "/static/media/ezekijel.6d0cc882.jpg"
+  },
+  {
+    "revision": "24a816ffeb52954ddb84fcd7c81ba27c",
+    "url": "/static/media/file.24a816ff.jpg"
+  },
+  {
+    "revision": "f8221f9af2987c402c4e62cfa75f59fc",
+    "url": "/static/media/ivona.f8221f9a.jpg"
+  },
+  {
+    "revision": "5add7c71713a6e0d51bc449711a86030",
+    "url": "/static/media/jarza.5add7c71.jpg"
+  },
+  {
+    "revision": "d47a7ee0d7663791b6063ddc93943d69",
+    "url": "/static/media/lorena.d47a7ee0.jpg"
+  },
+  {
+    "revision": "f6cef6edfe12f6b74106827dfa3a2a67",
+    "url": "/static/media/lovro.f6cef6ed.jpg"
+  },
+  {
+    "revision": "8ac111c8a51968e125fe58f167977143",
+    "url": "/static/media/morana.8ac111c8.jpg"
+  },
+  {
+    "revision": "3553a22586339a1ce9e9a03c95c6402c",
+    "url": "/static/media/petra.3553a225.jpg"
+  },
+  {
+    "revision": "265f2847308b0c1555112c256784a3f7",
+    "url": "/static/media/valentino.265f2847.jpg"
+  }
+]);
