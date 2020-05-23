5c30b948a6ee15372015201e89e9e15d0ee9d1fb
Ivan Jerzabek <jerzabek.ivan@gmail.com>
2020-05-23 19:45:01 +0200

    Wrap na landingu

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     <div className='stripes headerklasa d-flex justify-content-center align-items-center'>
       <div className='crnitekst big'>
         <h1 className='display-4 titl'>NSOUS Karantena</h1>
-        <div className='kutija d-flex flex-row align-items-baseline justify-content-end'>
+        <div className='kutija d-flex flex-row flex-wrap align-items-baseline justify-content-end'>
           <a href='https://www.instagram.com/naso_sam_ovo_u_smecu/' className='text-monospace'>@nasao_sam_ovo_u_smecu</a>
           <p className='mx-2'>•</p>
           <a href='https://www.instagram.com/ivanjerzabek/' className='text-monospace'>@ivanjerzabek</a>
